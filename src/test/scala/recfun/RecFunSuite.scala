7144e09d300c5d323e65f02f873e1e881c2afab7
Christian Morettin <christian.morettin@cgn.it>
2020-09-09 12:07:03 +0200

    Esercizio 1
    correzione balance
    
    aggiunto esercizio count change

--- src/test/scala/recfun/RecFunSuite.scala
+++ src/test/scala/recfun/RecFunSuite.scala
@@ -25,13 +25,13 @@ class RecFunSuite {
   @Test def `countChange: example given in instructions`: Unit =
     assertEquals(3, countChange(4,List(1,2)))
 
-  @Test def `countChange: sorted CHF`: Unit =
+    @Test def `countChange: sorted CHF`: Unit =
     assertEquals(1022, countChange(300,List(5,10,20,50,100,200,500)))
 
-  @Test def `countChange: no pennies`: Unit =
+   @Test def `countChange: no pennies`: Unit =
     assertEquals(0, countChange(301,List(5,10,20,50,100,200,500)))
 
-  @Test def `countChange: unsorted CHF`: Unit =
+   @Test def `countChange: unsorted CHF`: Unit =
     assertEquals(1022, countChange(300,List(500,5,50,100,20,200,10)))
 
   // ------ pascal tests ------------------------------------------------------
